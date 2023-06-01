df79f3070e1d18bbc8cd5a8279f33ca3f3a99146
SorenWi <soerenwinterhalder@gmail.com>
2023-06-01 20:35:00 +0200

    progress

--- ik.js
+++ ik.js
@@ -1,24 +1,25 @@
 import * as THREE from "three";
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { Joint } from "./joint.js" 
+import { VRButton } from 'three/examples/jsm/webxr/VRButton';
+import { XRControllerModelFactory } from 'three/examples/jsm/webxr/XRControllerModelFactory';
+import { Joint } from "./joint.js"
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 //Setup
 let scene, camera, renderer;
 let controls;
+let controllers;
+let armR, armL;
+let shoulderR, shoulderL, shoulderContainer;
+const shoulderROffset = new THREE.Vector3(1, -.3, 0);
+const shoulderLOffset = new THREE.Vector3(-1, -.3, 0);
 
-let plane;
+//Joints
+let jointsR = [];
+let jointsL = [];
+const jointLengths = [.9, .85, 0];
 
-//Time
-let prevTime = 0;
-
-//Joint settings
-const joints = [];
-const jointAmount = 5;
-const jointLength = 1;
-
-//Raycaster for target position
-let targetPosition = new THREE.Vector3(0, 0, 0);
-let pointer = {x: 0, y: 0};
+let pointer = { x: 0, y: 0 };
 const backgroundLayer = 1;
 const raycaster = new THREE.Raycaster();
 raycaster.layers.set(backgroundLayer);
@@ -28,35 +29,96 @@ function setup() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
-    
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
 
-    //Light
-    const ambientLight = new THREE.AmbientLight(0x404040);
-    scene.add(ambientLight);
+    //VR
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.xr.enabled = true;
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    document.body.appendChild(renderer.domElement);
+    let vrButton = VRButton.createButton(renderer)
+    document.body.appendChild(vrButton);
+    controllers = buildControllers();
+
+    //Lights
+	const ambientLight = new THREE.AmbientLight(0x606060, 2);
+	scene.add(ambientLight);
+
+	const pointLight = new THREE.PointLight(0x404040, 2, 50);
+	pointLight.position.setZ(3);
+	scene.add(pointLight);
 
     //Grid helper
-    const gridHelper = new THREE.GridHelper( 10, 10 );
-    scene.add( gridHelper );
+    const gridHelper = new THREE.GridHelper(10, 10);
+    scene.add(gridHelper);
 
     //Orbit control
     controls = new OrbitControls(camera, renderer.domElement);
     controls.update();
 
-    //Events
+    //Load arms
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load("armL.glb", (glb) => {
+        armL = glb.scene;
+        armL.scale.set(1, 1, 1);
+        armL.position.set(0, 0, 0);
+       
+        gltfLoader.load("armR.glb", (glb) => {
+            armR = glb.scene;
+            armR.scale.set(2, 2, 2);
+            armR.position.set(0, 0, 0);
+            onInitialized();
+        });
+    });
+   
+}
+
+function onInitialized() {
+    createJoints();
+    createShoulders();
+    renderer.setAnimationLoop(animate);
     window.addEventListener('pointermove', onPointerMove);
 }
 
-function createBackgroundPlane() {
-    const planeGeometry = new THREE.PlaneGeometry(100, 100);
-    const planeMaterial = new THREE.MeshStandardMaterial();
-    plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    //plane.visible = false;
-    plane.layers.enable(backgroundLayer);
+function createShoulders() {
+    shoulderContainer = new THREE.Mesh();
+    
+    shoulderR = new THREE.Mesh();
+    shoulderL = new THREE.Mesh();
 
-    scene.add(plane);
+    let shoulderPosR = new THREE.Vector3();
+    let shoulderPosL = new THREE.Vector3();
+
+    shoulderPosR.addVectors(shoulderContainer.position, shoulderROffset);
+    shoulderPosL.addVectors(shoulderContainer.position, shoulderLOffset);
+
+    shoulderR.position.set(shoulderPosR.x, shoulderPosR.y, shoulderPosR.z);
+    shoulderL.position.set(shoulderPosL.x, shoulderPosL.y, shoulderPosL.z);
+
+    shoulderContainer.add(shoulderR);
+    shoulderContainer.add(shoulderL);
+
+    scene.add(shoulderContainer);
+}
+
+function updateShoulders() {
+    shoulderContainer.position.set(camera.position.x, camera.position.y, camera.position.z);
+
+    shoulderContainer.rotation.y = camera.rotation.y;
+}
+
+
+//Create joints and add them to an array and to the scene
+function createJoints() {
+    for (let i = 0; i < 3; i++) {
+        let jointR = new Joint(jointLengths[i], armR.children[0]);
+        let jointL = new Joint(jointLengths[i], armL.children[0]);
+        jointsL.push(jointL)
+        jointsR.push(jointR);
+        scene.add(jointR.jointObject);
+        scene.add(jointL.jointObject);
+    }
 }
 
 function onPointerMove(event) {
@@ -64,67 +126,28 @@ function onPointerMove(event) {
 	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 }
 
-//Create joints and add them to an array and to the scene
-function createJoints() {
-    //Create joints except last one
-    for (let i = 0; i < jointAmount - 1; i++) {
-        //TODO replace with actual geometry
-        const jointGeometry = new THREE.SphereGeometry(.3);
-        const jointMaterial = new THREE.MeshStandardMaterial( {color: 0xFF00FF });
-        const jointMesh = new THREE.Mesh(jointGeometry, jointMaterial);
-        
-        const armGeometry = new THREE.CylinderGeometry(0.2, 0.2, 1);
-        const armMaterial = new THREE.MeshStandardMaterial( {color: 0xFFF});
-        const connectionObject = new THREE.Mesh(armGeometry, armMaterial);
-
-        let joint = new Joint(jointLength, jointMesh, connectionObject);
-        joints.push(joint);
-        scene.add(joint.jointObject);
-    }
-
-    //TODO replace with actual geometry
-    const jointGeometry = new THREE.SphereGeometry(.3);
-    const jointMaterial = new THREE.MeshStandardMaterial( {color: 0xFF00FF });
-    const jointMesh = new THREE.Mesh(jointGeometry, jointMaterial);
-
-    const armGeometry = new THREE.CylinderGeometry(0.2, 0.2, 0);
-    const armMaterial = new THREE.MeshStandardMaterial( {color: 0xFFF});
-    const connectionObject = new THREE.Mesh(armGeometry, armMaterial);
-
-    let lastJoint = new Joint(0, jointMesh, connectionObject);
-
-    joints.push(lastJoint);
-    scene.add(lastJoint.jointObject);
-}
-
-
+//Called every frame
 function animate(time) {
     requestAnimationFrame(animate);
-    time *= 0.001;
-	let deltaTime = time - prevTime;
 
-    update(time, deltaTime);
+    updateShoulders();
 
-    //Standard update
+    updateJoints(jointsR, shoulderR, controllers[0]);
+    updateJoints(jointsL, shoulderL, controllers[1]);
+
     controls.update();
-	renderer.render(scene, camera);
-	prevTime = time;
-
+    renderer.render(scene, camera);
 }
 
-function update(time, deltaTime) {
-    updateTargetPosition();
-    updateJoints()
-  
-}
+function updateJoints(joints, root, targetController) {
+    //Set last joint to target controllers position and rotation
+    const targetPosition = targetController.position;
+    const targetRotation = targetController.rotation;
+    joints[joints.length - 1].setPosition(targetController.position.x, targetPosition.y, targetPosition.z);
+    joints[joints.length -1].jointObject.rotation.set(targetRotation.x, targetRotation.y, targetRotation.z);
 
-function updateJoints() {
-   
-    //Set last joint to target position
-    joints[joints.length - 1].setPosition(targetPosition.x, targetPosition.y, targetPosition.z);
-
-    //Update joints
-    for(let i = joints.length - 2; i >= 0; i--) {
+    //Update position and rotation of joints
+    for (let i = joints.length - 2; i >= 0; i--) {
         //Update rotation
         joints[i].lookAt(joints[i + 1].getPosition());
 
@@ -132,22 +155,53 @@ function updateJoints() {
         const pos = joints[i + 1].getPosition();
         joints[i].setEndPosition(pos.x, pos.y, pos.z);
     }
-}
 
-function updateTargetPosition() {
-    //Get target position
-    raycaster.setFromCamera(pointer, camera);
+    //Offset joints so the shoulder (root joint) is pinned to a set position
+    let rootWorldPos = new THREE.Vector3();
+    root.getWorldPosition(rootWorldPos); 
 
-    const intersects = raycaster.intersectObjects(scene.children);
-    if (intersects.length > 0) {
-        targetPosition = intersects[0].point;
+    let offset = new THREE.Vector3();
+    offset.subVectors(joints[0].getPosition(), rootWorldPos);
+
+    for (let i = 0; i < joints.length; i++) {
+        let newPos = new THREE.Vector3();
+        newPos.subVectors(joints[i].getPosition(), offset);
+        joints[i].setPosition(newPos.x, newPos.y, newPos.z);
     }
 }
 
+
+// src: https://codingxr.com/articles/getting-started-with-webxr-and-threejs/
+function buildControllers() {
+    const controllerModelFactory = new XRControllerModelFactory();
+
+    const geometry = new THREE.BufferGeometry().setFromPoints([
+        new THREE.Vector3(0, 0, 0),
+        new THREE.Vector3(0, 0, -1)
+    ]);
+
+    const line = new THREE.Line(geometry);
+    line.scale.z = 10;
+
+    const controllers = [];
+
+    for (let i = 0; i < 2; i++) {
+        const controller = renderer.xr.getController(i);
+        controller.add(line.clone());
+        controller.userData.selectPressed = false;
+        controller.userData.selectPressedPrev = false;
+        scene.add(controller);
+        controllers.push(controller);
+
+        const grip = renderer.xr.getControllerGrip(i);
+        grip.add(controllerModelFactory.createControllerModel(grip));
+        scene.add(grip);
+    }
+
+    return controllers;
+}
+
 setup();
-createJoints();
-createBackgroundPlane();
-animate(0);
 
 
 
--- joint.js
+++ joint.js
@@ -1,25 +1,16 @@
 import * as THREE from 'three';
-
-//Joint contains 3 objects. 
-//Joint: Is the pivot and parent
-//Connection: Is just for the visuals
-//JointEnd: Helper to get the position at   
+   
 export class Joint {
     jointObject; 
-    connectionObject;
     jointEndObject;
 
     length;
 
-    constructor(length, jointObject, connectionObject) {
+    constructor(length, jointObject) {
         this.length = length;
         this.jointObject = jointObject;
-        this.connectionObject = connectionObject;
 
-        //Align so joint is at the beginning of the connection
-        this.connectionObject.rotation.x = -Math.PI * 0.5;
-        this.connectionObject.position.setZ(length * 0.5);
-       
+
         //Helper object to get the world position at the end of the joint
         const jointEndGeometry = new THREE.BoxGeometry(.1,.1,.1);
         const jointEndMaterial = new THREE.MeshStandardMaterial( {color: 0x000000 });
@@ -28,7 +19,6 @@ export class Joint {
         this.jointEndObject.position.setZ(length);
 
         //Set joint as parent of connection and helper object
-        this.jointObject.add(this.connectionObject);
         this.jointObject.add(this.jointEndObject);
     }
 
@@ -53,7 +43,6 @@ export class Joint {
     getEndPosition() {
         let handleVec = new THREE.Vector3();
         this.jointEndObject.getWorldPosition(handleVec);
-        handleVec.setZ(0);
         return handleVec;
     }
 
